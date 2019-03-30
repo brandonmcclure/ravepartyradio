04a677a955454e288c96f8a38e5e01fdf57b4d99
Colin <smithcolina@hotmail.com>
2019-03-30 10:47:52 +0000

    Update listeners & song scripts

--- site/listeners.php
+++ site/listeners.php
@@ -1,21 +1,9 @@
 <?php
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "s8.myradiostream.com:57132/7.html");
-curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-$data = curl_exec($ch);
-curl_close($ch);
-$data = str_replace('</body></html>', "", $data);
-$split = explode(',', $data);
-if (empty($data)) {
-	$listeners = 0;
-} else { 
-	if ($split[1] == "0") {
-		$listeners = 0;
-	} else {
-		$listeners = $split[0];
-	}
-}
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+$data = file_get_contents('http://icecast:8000/status-json.xsl');
+$json = json_decode($data, true);
+$listeners = $json['icestats']['source'][4]['listeners'];
 echo $listeners;
 ?>
--- site/song.php
+++ site/song.php
@@ -1,27 +1,9 @@
 <?php
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "s8.myradiostream.com:57132/7.html");
-    curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-    $data = curl_exec($ch);
-    curl_close($ch);
-    $data = str_replace('</body></html>', "", $data);
-    $split = explode(',', $data); 
-    if (empty($split[6])) { 
-        $title = "The current song is not available "; 
-    } else { 
-        $count = count($split); 
-        $i = "6"; 
-        while($i<=$count) { 
-            if ($i > 6) { 
-                $title .= "," . $split[$i]; 
-            } else { 
-                $title .= $split[$i]; 
-            } 
-            $i++; 
-        }
-    }
-    $title = substr($title, 0, -1);
-    echo $title;
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+$data = file_get_contents('http://icecast:8000/status-json.xsl');
+$json = json_decode($data, true);
+$title = $json['icestats']['source'][4]['title'];
+echo $title;
 ?>
