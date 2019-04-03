4fb14ba92699806332b1adef4bf16c9cb6cd3387
Colin <smithcolina@hotmail.com>
2019-04-03 08:00:32 +0100

    Fix bug in song.php

--- site/song.php
+++ site/song.php
@@ -5,5 +5,5 @@ error_reporting(E_ALL);
 $data = file_get_contents('http://icecast:8000/status-json.xsl');
 $json = json_decode($data, true);
 $title = $json['icestats']['source'][4]['title'];
-echo ".$title;
+echo $title;
 ?>
