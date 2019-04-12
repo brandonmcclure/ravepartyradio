53356646e5e709a02648f18f1d37662eb071dea4
Colin <smithcolina@hotmail.com>
2019-04-12 20:47:38 +0100

    Use ftpd not localhost

--- site/song.php
+++ site/song.php
@@ -2,6 +2,6 @@
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
-$data = file_get_contents('http://localhost:8024/data/latest.txt');
+$data = file_get_contents('http://ftpd/data/latest.txt');
 echo $data;
 ?>
