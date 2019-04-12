c93a75852e1f010ac60cf347e54afe792e758055
Colin <smithcolina@hotmail.com>
2019-04-12 20:44:41 +0100

    Display latest.txt as song name

--- site/song.php
+++ site/song.php
@@ -2,8 +2,6 @@
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
-$data = file_get_contents('http://icecast:8000/status-json.xsl');
-$json = json_decode($data, true);
-$title = $json['icestats']['source'][4]['title'];
-echo $title;
+$data = file_get_contents('http://localhost:8024/data/latest.txt');
+echo $data;
 ?>
