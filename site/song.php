59fc3649dacd738a7a1228a0ffdee9ff5735e7fb
Colin <smithcolina@hotmail.com>
2019-04-12 20:51:09 +0100

    Use webftp

--- site/song.php
+++ site/song.php
@@ -2,6 +2,6 @@
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
-$data = file_get_contents('http://ftpd:8024/data/latest.txt');
+$data = file_get_contents('http://webftp:8024/data/latest.txt');
 echo $data;
 ?>
