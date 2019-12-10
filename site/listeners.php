5e0122a9ff7c458be8e2cd0aa9ff49673e46d711
Colin <smithcolina@hotmail.com>
2019-12-10 20:49:55 +0000

    Fix listener count after removing some mounts

--- site/listeners.php
+++ site/listeners.php
@@ -4,6 +4,11 @@ error_reporting(E_ALL);
 
 $data = file_get_contents('http://icecast:8000/status-json.xsl');
 $json = json_decode($data, true);
-$listeners = $json['icestats']['source'][3]['listeners'];
-echo "<p>$listeners</p>";
+$title = $json['icestats']['source']['title'];
+$listeners = $json['icestats']['source']['listeners'];
+$listener_peak = $json['icestats']['source']['listener_peak'];
+
+echo "<p>Now playing: $title</p>";
+echo "<p>Current Listeners: $listeners</p>";
+echo "<p>Peak Listeners: $listener_peak</p>";
 ?>
